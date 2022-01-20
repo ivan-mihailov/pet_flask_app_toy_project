6e5b706b1ec48a499431445aee93a903dd716acf
ivan-mihailov <ivan.i.mihailov@gmail.com>
2022-01-21 00:28:12 +0100

    Final refactor to app.py, add templates for all API routes, modifications to sqlite db

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, flash, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+import simplejson
+from flask import Flask, render_template, flash, request
 from flask_bootstrap import Bootstrap
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, RadioField
 
 """Create and configure an instance of the Flask application."""
 app = Flask(__name__)
@@ -24,23 +25,32 @@ class Pet(db.Model):
     Name = db.Column(db.String(100), nullable=False)  # Stores Pet name in sqlite table
     Breed = db.Column(db.String(3), nullable=False)  # Stores Pet breed in sqlite table (can be only "dog" or "cat")
 
+    def to_json(pet) -> str:
+        if isinstance(pet, __builtins__.list):
+            pet_schema = PetSchema(many=True)
+            output = pet_schema.dump(pet)
+            return simplejson.dumps(output)
+        else:
+            pet_schema = PetSchema()
+            output = pet_schema.dump(pet)
+            return simplejson.dumps(output)
+
 
 class PetSchema(ma.Schema):
     class Meta:
         model = Pet
         fields = ("Name", "Breed")
 
-    def to_json(self):
-        pets = Pet.query.all()
-        pet_schema = PetSchema(many=True)
-        output = pet_schema.dump(pets)
-        return jsonify(output)
-
 
 class AddRecord(FlaskForm):
     pet_name = StringField('Pet Name')
-    breed_name = StringField('Breed Name')
+    breed_name = RadioField('Breed Name', choices=[('Dog', 'Dog'), ('Cat', 'Cat')])
     submit = SubmitField('Add Record')
+
+
+class GetName(FlaskForm):
+    pet_name = StringField('Pet Name')
+    submit = SubmitField('Submit')
 
 
 db.init_app(app)
@@ -48,18 +58,38 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return 'Welcome to the Pet Tracking App'
+    return render_template('index.html')
 
 
 @app.route('/newestpet', methods=['GET'])
-def get_newest_pet():
-    return Pet.query.order_by(Pet.ID.desc()).first()
+def newest_pet():
+    newest_pet = Pet.query.order_by(Pet.ID.desc()).first()
+    if newest_pet:
+        #message = type(newest_pet)
+        message = f"The newest pet in the app is {Pet.to_json(newest_pet)}."
+    else:
+        message = f"There are no pets in the app."
+    return render_template('newest_pet.html', message=message)
 
 
-@app.route('/petwithname', methods=['GET'])
-def get_pet_with_name():
-    pet_name = request.form.get('pet_name')
-    return Pet.query.filter_by(Name=pet_name).all()
+@app.route('/petwithname', methods=['GET', 'POST'])
+def pet_with_name():
+    form1 = GetName()
+    if form1.validate_on_submit():
+        pet_name = request.form.get('pet_name')
+        pet_with_name = Pet.query.filter_by(Name=pet_name).all()
+        #message = type(pet_with_name)
+        message = f"The pets with name {pet_name} are as follows: {Pet.to_json(pet_with_name)}"
+        return render_template('pet_with_name.html', form1=form1, message=message)
+    else:
+        # show validation errors
+        for field, errors in form1.errors.items():
+            for error in errors:
+                flash("Error in {}: {}".format(
+                    getattr(form1, field).label.text,
+                    error
+                ), 'error')
+        return render_template('pet_with_name.html', form1=form1)
 
 
 @app.route('/addpet', methods=['GET', 'POST'])
@@ -85,6 +115,7 @@ def add_pet():
 
 
 if __name__ == '__main__':
+    # db.drop_all() # Uncomment line if you want the database to be purged every time app is started
     db.create_all()
     app.debug = True
     app.run()
